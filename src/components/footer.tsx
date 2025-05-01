f5bfaf2085549444b2831438cc2967b392cc1243
PalakJain1504 <palakjain03373@gmail.com>
2025-05-02 00:15:21 +0530

    footer addition

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className="py-6 px-4 bg-gray-100 dark:bg-gray-800 border-t border-gray-200 dark:border-gray-700 mt-auto">
       <div className="container mx-auto text-center text-gray-600 dark:text-gray-400 text-sm">
-        <p>Designed and Developed by Palak Jain and Purvi Singhal [B.Sc. CS & DS] | © 2025 Project Portal</p>
+        <p>Designed and Developed by Palak Jain and Purvi Singhal [B.Sc. CS & DS] | © 2025 Project And Internship Management Portal- SOET, KRMU</p>
       </div>
     </footer>
   );
